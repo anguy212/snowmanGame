03750c4d6be9b712cf90ee7203803cfcba14a0ad
anguy212 <32521966+anguy212@users.noreply.github.com>
2019-05-26 17:10:22 -0700

    triggers when touching fire and snow working

--- trainingModule/Assets/playerMovement.cs
+++ trainingModule/Assets/playerMovement.cs
@@ -9,20 +9,33 @@ public class playerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0f, -4.05f, -0.6572319f);
+        transform.position = new Vector3(0f, -4.05f, -0f);
     }
     // Update is called once per frame
     void Update()
     {
         mousePos = Input.mousePosition.x;
-        if(mousePos < - 23.4f)
+        if (mousePos < -23.4f)
         {
             mousePos = -23.4f;
         }
-        else if(mousePos > 23.4f)
+        else if (mousePos > 23.4f)
         {
             mousePos = 23.4f;
         }
-        transform.position = new Vector3(mousePos, -35.25f, -0.6572319f);
+        transform.position = new Vector3(mousePos, -35.25f, -0f);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("fire"))
+        {
+            Debug.Log("GameObject1 collided with " + collision.name);
+            collision.transform.position = new Vector3(Random.Range(-23f, 23f), 45f, -0f);
+        }
+        else if (collision.gameObject.CompareTag("snow"))
+        {
+            Debug.Log("GameObject1 collided with " + collision.name);
+            collision.transform.position = new Vector3(Random.Range(-23f, 23f), 45f, -0f);
+        }
     }
 }
